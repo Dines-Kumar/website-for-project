4039055852009804a030484940d25c60c48a50e8
Dines-Kumar <onegoal009@gmail.com>
2024-01-09 22:12:55 +0800

    made changes

---
+++ signout.js
@@ -0,0 +1,25 @@
+<script type="module">
+    
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDReSRfUa3_jd5JHbyyoW6_5K6-w6yq2Q4",
+    authDomain: "apptalk-80117.firebaseapp.com",
+    projectId: "apptalk-80117",
+    storageBucket: "apptalk-80117.appspot.com",
+    messagingSenderId: "200785318528",
+    appId: "1:200785318528:web:c85f7154fddea7195f0c65",
+    measurementId: "G-WR4PWL049J"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
+</script>
